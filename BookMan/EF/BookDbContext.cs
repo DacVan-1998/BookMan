5a733f336c3cf32860a38f5edf9a29bbf2dd4564
Dac Van <van264661@nuce.edu.vn>
2020-11-06 10:55:52 +0700

    Configuration entity with Data annotation attribute

--- BookMan/EF/BookDbContext.cs
+++ BookMan/EF/BookDbContext.cs
@@ -9,12 +9,28 @@ namespace BookMan.EF
 {
     public class BookDbContext : DbContext
     {
+        public const string con_str = @"Data Source=DESKTOP-TC51ULG\SQLEXPRESS;Initial Catalog=Book;User ID=SA;Password=anhvan123";
+
         public BookDbContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Book> Books { get; set; }
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
 
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(con_str);           // thiết lập làm việc với SqlServer
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+              new Book() { Id = 1, Name = "ASP.NET Core for dummy", Authors = "Trump D.", Publisher = "Washington", Year = 2020, Description = "Đây là của Trump" },
+              new Book() { Id = 2, Name = "Pro ASP.NET Core", Authors = "Putin V. D.", Publisher = "Moscow", Year = 2020, Description = "Đây là của Putin" },
+              new Book() { Id = 3, Name = "ASP.NET Core Video Tutorial  ", Authors = "Obama B.", Publisher = "Washington", Year = 2020, Description = "Đây là của Obama" }
+              );
+        }
     }
+
 }
