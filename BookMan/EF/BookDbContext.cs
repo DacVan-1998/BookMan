97419896506c08ed0c21dd1b1ffb07c9b92e97b8
Dac Van <van264661@nuce.edu.vn>
2020-11-06 10:47:30 +0700

    Add entity

---
+++ BookMan/EF/BookDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookMan.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookMan.EF
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        
+
+    }
+}
---
+++ BookMan/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookMan.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, DisplayName("Tiêu đề")]
+        public string Name { get; set; }
+
+        [Required, DisplayName("Tác giả")]
+        public string Authors { get; set; }
+
+        [Required, DisplayName("Nhà xuất bản")]
+        public string Publisher { get; set; }
+
+        [Required, DisplayName("Năm xuất bản"), Range(1990, 2020)]
+        public int Year { get; set; }
+
+        [DisplayName("Tóm tắt")]
+        public string Description { get; set; }
+    }
+}
