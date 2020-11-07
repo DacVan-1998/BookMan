20cd62f40904ec0c27b588a9524fccb14cfb9dae
Dac Van <van264661@nuce.edu.vn>
2020-11-07 10:04:20 +0700

    CRUD for Book

--- BookMan/Controllers/HomeController.cs
+++ BookMan/Controllers/HomeController.cs
@@ -6,24 +6,98 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BookMan.Models;
+using Microsoft.EntityFrameworkCore;
+using BookMan.EF;
 
 namespace BookMan.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BookDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BookDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var listBook = _context.Books.ToList();
+            return View(listBook);
+        }
+     
+        public IActionResult Create()
+        {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return View();
+
+            }
+            return Redirect("/Home/Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = _context.Books.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(book);
+                _context.SaveChanges();
+                ViewBag.Mess = "Sửa thành công";
+            }
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = _context.Books.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Books.Find(id);
+            _context.Books.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult About()
         {
             return View();
         }
